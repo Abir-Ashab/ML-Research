744ab99b370c58ae5d4b0fce9aea5df21bb6dcdd
Abir-Ashab <bsse1315@iit.du.ac.bd>
2025-06-03 21:18:27 +0600

    add python file

---
+++ main.py
@@ -0,0 +1,13 @@
+import nibabel as nib
+
+# Load the NIfTI file
+img = nib.load('sub-0001_t2.nii.gz')
+
+# Print the header
+print(img.header)
+
+print("Shape:", img.shape)
+print("Spacing:", img.header.get_zooms())
+
+# print slice thickness
+print("Slice thickness:", img.header.get_zooms()[-1])
